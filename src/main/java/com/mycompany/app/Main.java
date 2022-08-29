6bc2bf9e88d9881330f06ef4223e509a32a6c887
lpdamin <lakshmidevops95@gmail.com>
2022-08-29 11:20:52 +0530

    config file addedd

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -4,17 +4,25 @@ import com.hashicorp.cdktf.App;
 import com.hashicorp.cdktf.NamedRemoteWorkspace;
 import com.hashicorp.cdktf.RemoteBackend;
 import com.hashicorp.cdktf.RemoteBackendProps;
+import com.hashicorp.cdktf.S3Backend;
+import com.hashicorp.cdktf.S3BackendProps;
 import com.hashicorp.cdktf.TerraformStack;
+import com.hashicorp.cdktf.providers.aws.s3.S3AccessPoint;
 
 public class Main
 {
     public static void main(String[] args) {
         final App app = new App();
         TerraformStack stack = new MainStack(app, "aws_instance");
+        /*new S3Backend(stack, S3BackendProps.builder()
+        .bucket("lakshmi-tfstate")
+        .region("us-east-1")
+        .build());*/
         new RemoteBackend(stack, RemoteBackendProps.builder()
             .hostname("app.terraform.io")
             .organization("lakshmi-www-prod")
             .workspaces(new NamedRemoteWorkspace("aws-ec2"))
+            .token("Zn54BEPUOs0YYQ.atlasv1.DobGOLLLbL51DCidu5La9CQ4EDHi6Lsi21Oo5cXACOd97SvBRqS31jPyBmBME80uBeA")
             .build());
 
         app.synth();
